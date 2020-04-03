371f34a51f1157b2253357e53a28b28426984a2d
Jung Hun <manbalboy@hanmail.net>
2020-04-04 02:18:08 +0900

    템플릿메소드 적용

--- study02/src/main/java/springbook/user/dao/UserDao.java
+++ study02/src/main/java/springbook/user/dao/UserDao.java
@@ -25,18 +25,10 @@ public class UserDao {
 	}
 
 	public void add(User user) throws ClassNotFoundException, SQLException {
-		Connection c = dataSource.getConnection();
-
-		PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values (?, ?, ?)");
-		ps.setString(1, user.getId());
-		ps.setString(2, user.getName());
-		ps.setString(3, user.getPassword());
-
-		ps.executeUpdate();
-
-		ps.close();
-		c.close();
+		StatementStrategy st = new AddStatement(user);
+		jdbcContextWithStatementStrategy(st);
 	}
+
 
 	public User get(String id) throws ClassNotFoundException, SQLException {
 		Connection c = dataSource.getConnection();
@@ -45,7 +37,6 @@ public class UserDao {
 		ps.setString(1, id);
 
 		ResultSet rs = ps.executeQuery();
-
 
 		User user = null;
 
@@ -67,32 +58,85 @@ public class UserDao {
 	}
 
 	public void deleteAll() throws ClassNotFoundException, SQLException {
-		Connection c = dataSource.getConnection();
+		StatementStrategy st = new DeleteAllStatement(); // 선정한 전략 클래스의 오브젝트 생성
+		jdbcContextWithStatementStrategy(st); // 컨텍스트 호출 전략 오브젝트 전달
+	}
 
-		PreparedStatement ps = c.prepareStatement("delete from users");
-		ps.executeUpdate();
+	public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
+		Connection c = null ;
+		PreparedStatement ps = null;
 
+		try {
+			c = dataSource.getConnection();
 
-		ps.close();
-		c.close();
+			ps = stmt.makePreparedStatement(c);
+			ps.executeUpdate();
+
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			if (ps!=null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+
+				}
+			}
+
+			if (c != null) {
+				try {
+					c.close();
+				} catch (SQLException e) {
+
+				}
+			}
+		}
 
 	}
 
+
+	public PreparedStatement makeStatement(Connection c) throws SQLException {
+		PreparedStatement ps = c.prepareStatement("delete from users");
+		return ps;
+	};
+
 	public int getCount() throws ClassNotFoundException, SQLException {
-		Connection c = dataSource.getConnection();
+		Connection c = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try {
+			c = dataSource.getConnection();
+			ps = c.prepareStatement("select count(*) from users");
+			rs = ps.executeQuery();
+			rs.next();
 
-		PreparedStatement ps = c.prepareStatement("select count(*) from users");
-		ResultSet rs = ps.executeQuery();
-		rs.next();
+			return rs.getInt(1);
+		} catch (SQLException e) {
+			throw e ;
+		} finally {
+			if (rs!=null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
 
-		int count = rs.getInt(1);
+				}
+			}
 
-		rs.close();
-		ps.close();
-		c.close();
+			if (ps!=null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
 
-		return count;
+				}
+			}
 
+			if (c != null) {
+				try {
+					c.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
 	}
 
 
